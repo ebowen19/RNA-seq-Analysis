d62b2f0f68b76928dce12c62cbf00678b21c59c7
Elizabeth <ebowen19@g.ucla.edu>
2024-01-17 14:41:09 -0800

    add csv & html files for preliminary PCA & volc unique genes

--- volc_PCA_html_creation.py
+++ volc_PCA_html_creation.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-from bs4 import BeautifulSoup
 
 #%%
 
@@ -9,7 +8,6 @@ root_folder = ('/Users/elizabeth 1/Library/CloudStorage/Box-Box/Wu Lab/Project -
 os.chdir(root_folder)
 csv_files = {}
 
-# Loop through all files in the directory
 # Loop through all files in the directory
 for filename in os.listdir(root_folder):
     # Check if the file is a CSV file
@@ -64,3 +62,36 @@ html_string_with_title = f"<h1>{file_title}</h1>\n{html_string}"
 with open(html_file_path, 'w') as f:
     f.write(html_string_with_title)
 #%%
+for filename in os.listdir(root_folder):
+    # Check if the file is a CSV file
+    if filename.endswith('.csv'):
+        # Construct the full path to the CSV file
+        csv_file_path = os.path.join(root_folder, filename)
+        # Read the CSV file
+        df = pd.read_csv(csv_file_path)
+        csv_files[filename] = df
+#%%
+gene_list = csv_files['46 Overlapping Genes.csv']['Gene ID ']
+df_PCA = csv_files['PCA Top 200 genes.csv']
+PCA_genes = df_PCA['Gene ID']
+
+# Using set difference to find unique genes
+PCA_unique_genes = set(PCA_genes) - set(gene_list)
+# Create a mask for unique genes
+PCA_unique_df = df_PCA[PCA_genes.isin(PCA_unique_genes)]
+
+#%%
+df_volc = csv_files['sensi_vs_non_total_genes.csv']
+volc_genes = df_volc['Gene ID']
+
+# Using set difference to find unique genes
+volc_unique_genes = set(volc_genes) - set(gene_list)
+# Create a mask for unique genes
+volc_unique_df = df_volc[volc_genes.isin(volc_unique_genes)]
+# next do the same to find the unique volcano plot genes (maybe try refactoring into function/loop)
+# save results to csv & html file & push to github then link to venn diagram
+#%%
+volc_unique_df.to_csv('volc_unique.csv', index=False)
+volc_unique_df.to_html('volc_unique.html', index=False)
+PCA_unique_df.to_csv('PCA_unique.csv', index=False)
+PCA_unique_df.to_html('PCA_unique.html', index=False)
