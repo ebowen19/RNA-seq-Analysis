2bf7152659dfb5934a74d385b3ec47f10c76d073
Elizabeth <ebowen19@g.ucla.edu>
2024-01-16 17:26:42 -0800

    add csv & html files for preliminary volcano & PCA Data

--- volc_PCA_html_creation.py
+++ volc_PCA_html_creation.py
@@ -41,3 +41,26 @@ for filename in os.listdir(root_folder):
 
 
 #%%
+filename = 'sensi_vs_non_total_genes.csv'
+csv_file_path = os.path.join(root_folder, filename)
+# Read the CSV file
+df = pd.read_csv(csv_file_path)
+
+# Convert the CSV file name to HTML file name
+html_filename = filename.replace('.csv', '.html')
+# Construct the full path to the HTML file
+html_file_path = os.path.join(root_folder, html_filename)
+
+# Extract the base filename without the .csv extension to use as a title
+file_title = os.path.splitext(filename)[0]
+
+# Convert the DataFrame to HTML
+html_string = df.to_html(index=False)
+
+# Add a heading to the HTML with the file title
+html_string_with_title = f"<h1>{file_title}</h1>\n{html_string}"
+
+# Write the HTML content to a file
+with open(html_file_path, 'w') as f:
+    f.write(html_string_with_title)
+#%%
